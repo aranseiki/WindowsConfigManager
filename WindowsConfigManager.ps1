53b2b0719f8db4e60f3be1ec4bb0a281ab53906f
Allan de Oliveira Almeida <arumeidaaran@outlook.com>
2024-10-18 02:21:53 -0300

    Applying DRY, creation of Hash config for assets, new validation for change execution and add notification.

--- WindowsConfigManager.ps1
+++ WindowsConfigManager.ps1
@@ -3,6 +3,7 @@ Import-Module "$PSScriptRoot/src/Confirm-Parameter.psm1" -Force
 Import-Module "$PSScriptRoot/src/Get-ConfigParameters.psm1" -Force
 Import-Module "$PSScriptRoot/src/Get-UtilityFunctions.psm1" -Force
 Import-Module "$PSScriptRoot/src/Manage-Access.psm1" -Force
+Import-Module "$PSScriptRoot/src/Send-Notification.psm1" -Force
 
 # Configuration file path and filename for storing configuration settings
 $ConfigFilePath = "$PSScriptRoot/config"
@@ -23,20 +24,50 @@ $MicrophoneVerbose = Convert-Value -Value $MicrophoneVerbose
 $CameraTask = Convert-Value -Value $CameraTask
 $CameraVerbose = Convert-Value -Value $CameraVerbose
 
+$AssetsData = @(
+    @{
+        'Name' = 'Microphone'
+        'AssetPath' = 'HKLM:\Software\Microsoft\Windows\CurrentVersion\CapabilityAccessManager\ConsentStore\microphone'
+        'NotificationData' = @{
+            'Title' = 'Configuration Changed'
+            'Message' = 'The microphone has been activated!'
+            'Icon' = "$env:SystemRoot\System32\shell32.dll"
+        }
+    },
+    @{
+        'Name' = 'Camera'
+        'AssetPath' = 'HKLM:\Software\Microsoft\Windows\CurrentVersion\CapabilityAccessManager\ConsentStore\webcam'
+        'NotificationData' = @{
+            'Title' = 'Configuration Changed'
+            'Message' = 'The camera has been activated!'
+            'Icon' = "$env:SystemRoot\System32\shell32.dll"
+        }
+    }
+)
+
 # Clear the console to make the output cleaner.
 Clear-Host
 
-# Header for microphone section
-Write-Host `n "Microphone: " `n
+foreach ($CurrentAsset in $AssetsData) {
+    $CurrentTask = $(Get-Variable -Include "$($CurrentAsset.Name)Task").Value
+    $CurrentVerbose = $(Get-Variable -Include "$($CurrentAsset.Name)Verbose").Value
 
-# Use the new function for the microphone
-$microphonePath = "HKLM:\Software\Microsoft\Windows\CurrentVersion\CapabilityAccessManager\ConsentStore\microphone"
-Set-AccessForItem -Path $microphonePath -Action $MicrophoneTask -Verbose $MicrophoneVerbose
+    # Header for microphone section
+    Write-Host `n "$($CurrentAsset.Name): " `n
+    $microphonePath = $CurrentAsset.AssetPath
+    $CurrentAccessValue = Get-AccessPropertyItem -Path $microphonePath -Verbose $CurrentVerbose
+    $CurrentIconObj = [System.Drawing.Icon]::ExtractAssociatedIcon($CurrentAsset.NotificationData.Icon)
 
-# Header for Camera section
-Write-Host `n "Camera: " `n
-
-# Use the new function for the Camera
-$CameraPath = "HKLM:\Software\Microsoft\Windows\CurrentVersion\CapabilityAccessManager\ConsentStore\webcam"
-Set-AccessForItem -Path $CameraPath -Action $CameraTask -Verbose $CameraVerbose
-
+    if (
+        (($CurrentAccessValue.ToUpper() -eq 'DENY') -and ($CurrentTask.ToUpper() -eq 'ALLOW')) -or
+        (($CurrentAccessValue.ToUpper() -eq 'ALLOW') -and ($CurrentTask.ToUpper() -eq 'DENY'))
+    ) {
+        # Use the new function for the microphone
+        Set-AccessForItem -Path $microphonePath -Action $CurrentTask -Verbose $CurrentVerbose
+        Send-Notification `
+            -Title $CurrentAsset.NotificationData.Title `
+            -Message $CurrentAsset.NotificationData.Message `
+            -Duration 3000 `
+            -Icon $CurrentIconObj
+    }
+}
