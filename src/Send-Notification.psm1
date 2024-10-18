b95a5b401a87a0ff94ca7677006e2c0ed1771434
Allan de Oliveira Almeida <arumeidaaran@outlook.com>
2024-10-18 02:16:03 -0300

    Fixing structure and parameters

--- src/Send-Notification.psm1
+++ src/Send-Notification.psm1
@@ -1,30 +1,33 @@
-﻿Arquivo módulo Send-Notification:
-function Send-Notification {
+﻿function Send-Notification {
     param (
-        [string]$Title = "Alteração Detectada",
-        [string]$Message = "Configuração do sistema foi modificada.",
-        [int]$Duration = 3000  # Duração da notificação em milissegundos
+        [string]$Title,
+        [string]$Message,
+        [int]$Duration,
+        [System.Drawing.Icon]$Icon
     )
 
-    # Carregar o tipo necessário
-    Add-Type -AssemblyName System.Windows.Forms
+    try {
+        # Carregar o tipo necessário
+        Add-Type -AssemblyName System.Windows.Forms
 
-    # Criar e configurar o ícone de notificação (usando variável estática)
-    if (-not $script:NotifyIcon) {
-        $script:NotifyIcon = New-Object System.Windows.Forms.NotifyIcon
-        $script:NotifyIcon.Icon = [System.Drawing.Icon]::ExtractAssociatedIcon("$env:SystemRoot\System32\shell32.dll")
+        # Criar e configurar o ícone de notificação (usando variável estática)
+        if (-not $script:NotifyIcon) {
+            $script:NotifyIcon = New-Object System.Windows.Forms.NotifyIcon
+        }
+        
+        $script:NotifyIcon.Icon = $Icon
+        $script:NotifyIcon.BalloonTipTitle = $Title
+        $script:NotifyIcon.BalloonTipText = $Message
         $script:NotifyIcon.Visible = $true
+        $script:NotifyIcon.ShowBalloonTip($Duration)
+
+        # Esperar a duração da notificação
+        Start-Sleep -Milliseconds $Duration
+    } finally {
+        if ($script:NotifyIcon) {
+            $script:NotifyIcon.Dispose()
+        }
     }
-
-    $script:NotifyIcon.Visible = $true
-
-    # Configurar e exibir a notificação
-    $script:NotifyIcon.BalloonTipTitle = $Title
-    $script:NotifyIcon.BalloonTipText = $Message
-    $script:NotifyIcon.ShowBalloonTip($Duration)
-
-    # Esperar a duração da notificação
-    Start-Sleep -Milliseconds $Duration
 }
 
 Export-ModuleMember -Function Send-Notification
