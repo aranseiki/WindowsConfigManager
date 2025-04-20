c57c6080b58ed1c4ec7c97f79b655c75c146095a
Allan de Oliveira Almeida <arumeidaaran@outlook.com>
2025-04-20 13:38:03 -0300

    Adding root path environment variable

--- WindowsConfigManager.ps1
+++ WindowsConfigManager.ps1
@@ -1,6 +1,11 @@
 ﻿# Clear the console to make the output cleaner.
 Clear-Host
 
+# Set the root path for the project
+$env:WindowsConfigManagerRootPath = "$PSScriptRoot"
+
+# While loop to keep the script running
 while ($true) {
+    # Importing the main script
     Import-Module "$PSScriptRoot/scripts/MainRules.ps1" -Force
 }
